3d66354a5c4771c8fe8833c487b5d6ae45bd4c4e
IgnacioSaura <ignaciosaura1891@gmail.com>
2018-12-10 11:13:31 -0200

    Maven
    Maven Example

---
+++ MavenExample/src/main/java/com/globant/bootcamps/SimpleSampleApplication.java
@@ -0,0 +1,12 @@
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class SimpleSampleApplication {
+
+    private static final Logger LOGGER = LogManager.getLogger(SimpleSampleApplication.class);
+
+    public static void main(final String[] args) {
+        LOGGER.info("LOOK MAMA, I'M RUNNING!");
+    }
+}
